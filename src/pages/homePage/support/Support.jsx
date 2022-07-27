96a3129c1d0692913463db871120e030d7ced8df
rifatjahan823 <rifatjahan823@gmail.com>
2022-07-27 16:46:21 +0600

    add support section

--- src/pages/homePage/support/Support.jsx
+++ src/pages/homePage/support/Support.jsx
@@ -1,8 +1,90 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import logoslide1 from '../../../assets/logo-slider-1.png';
+import logoslide2 from '../../../assets/logo-slider-2.png';
+import logoslide3 from '../../../assets/logo-slider-3.png';
+import logoslide4 from '../../../assets/logo-slider-4.png';
+import logoslide5 from '../../../assets/logo-slider-5.png';
+import './Support.css'
+
 
 const Support = () => {
   return (
-    <div>Support</div>
+    <div className='support-section'>
+      <div className='container'>
+        <h2>Supported by</h2>
+        <div className='row'>
+          <div className='col-12'>
+            <div className='slide-logo'>
+              {/* ---------for desktop--------- */}
+              <Swiper className='d-none d-lg-block'
+                spaceBetween={5}
+                slidesPerView={5}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                modules={[Autoplay, Pagination]}
+                navigation
+                loop={true}
+                 autoplay={{ delay: 3000 }}
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+              >
+                <SwiperSlide ><img src={logoslide1} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide2} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide3} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide4} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide5} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide3} alt="" /></SwiperSlide>
+              </Swiper>
+              {/* ---------for tab--------- */}
+              <Swiper className='d-none d-lg-none d-md-block'
+                spaceBetween={50}
+                slidesPerView={2}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                modules={[Autoplay,Pagination]}
+                navigation
+                loop={true}
+                autoplay={{ delay: 3000 }}
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+              >
+                <SwiperSlide ><img src={logoslide1} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide2} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide3} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide4} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide5} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide3} alt="" /></SwiperSlide>
+              </Swiper>
+              {/* ---------for mobile--------- */}
+              <Swiper className='d-block d-md-none'
+                spaceBetween={50}
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                modules={[Autoplay,Pagination]}
+                navigation
+                loop={true}
+                autoplay={{ delay: 3000 }}
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+              >
+                <SwiperSlide ><img src={logoslide1} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide2} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide3} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide4} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide5} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={logoslide3} alt="" /></SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/shared/footer/Footer.jsx
+++ src/pages/shared/footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { TiSocialFacebook,TiSocialTwitter,TiSocialLinkedin,TiSocialYoutube } from "react-icons/ti";
 import { IoLogoInstagram } from "react-icons/io";
-import './Footer.css'
+import './Footer.css';
 import { Link } from 'react-router-dom';
+import footerlogo from '../../../assets/Group 42.png'
 
 
 const Footer = () => {
@@ -10,13 +11,13 @@ const Footer = () => {
     <div className='footer-section'>
         <div className='container'>
           {/* ------------footer-top--------- */}
-            <div className='row align-items-center justify-content-center'>
+            <div className='row align-items-center justify-content-center gy-4 mb-5'>
                 <div className='col-md-6 col-12'>
-                   
+                   <img src={footerlogo } alt="" />
                 </div>
                 <div className='col-md-6 col-12'>
                   {/* -------------icon-area------- */}
-                    <div className='d-flex justify-content-md-end justify-content-center align-items-center'>
+                    <div className='d-flex justify-content-md-end justify-content-start align-items-center'>
                         <p>follow us</p>
                         <TiSocialTwitter className='footer-top-icon'/>
                         <IoLogoInstagram className='footer-top-icon'/>
@@ -25,7 +26,7 @@ const Footer = () => {
                         <TiSocialYoutube className='footer-top-icon'/>
                     </div>
                     {/* -----------link-area---------- */}
-                    <div className='d-flex justify-content-md-end justify-content-center align-items-center mt-3'>
+                    <div className='d-flex justify-content-md-end justify-content-start align-items-center mt-3 '>
                         <Link to=''>How it works</Link>
                         <Link to=''>Search teachers</Link>
                         <Link to=''>About us</Link>
@@ -34,12 +35,12 @@ const Footer = () => {
                 </div>
             </div>
           {/* ------------footer-bottom--------- */}
-          <div className='row  justify-content-center  align-items-center mt-5'>
-                <div className='col-md-6 col-12 text-center text-md-start'>
+          <div className='row gy-3 justify-content-start align-items-center'>
+                <div className='col-md-6 col-12 text-start text-md-start order-2 order-md-1'>
                     <p>&copy; 2022 Teachers' Directory. All rights reserved.</p>
                 </div>
-                <div className='col-md-6 col-12 '>
-                     <div className='d-flex justify-content-md-end justify-content-center align-items-center mt-3'>
+                <div className='col-md-6 col-12 order-1 order-md-2'>
+                     <div className='d-flex justify-content-md-end justify-content-start align-items-center mt-3'>
                         <Link to=''>Terms & Conditions</Link>
                         <Link to=''>Privacy Policy</Link>
                     </div> 
