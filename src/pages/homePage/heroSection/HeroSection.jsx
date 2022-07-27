1289e9a816cc8e35fba22dd096e9534fdc309fe2
rifatjahan823 <rifatjahan823@gmail.com>
2022-07-28 02:53:37 +0600

    add hero section

--- src/pages/homePage/heroSection/HeroSection.jsx
+++ src/pages/homePage/heroSection/HeroSection.jsx
@@ -7,8 +7,29 @@ const HeroSection = () => {
         <div className='container'>
             <h1>The UK's First Teachers' Directory</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec dolor condimentum sapien placerat fringilla et luctus massa.</p>
-            <div>
-              
+            <div className='d-flex justify-content-center  search-filed'>
+                <div >
+                <label htmlFor="">Search words</label><br />
+                <input type="text" name="" id="" />
+                </div>
+               <div className='mx-2'>
+               <label htmlFor="">location</label><br />
+                <input type="text" name="" id="" />
+               </div>
+               <div>
+               <button className='common-btn input-btn'>Search</button>
+               </div>
+            </div>
+            <div className='row hero-bottom g-4 justify-content-center'>
+                <div className='col-lg-4 col-md-6 col-12'>
+                    <p>Connect with schools</p>
+                </div>
+                <div className='col-lg-4 col-md-6 col-12'>
+                    <p>Build your network</p>
+                </div>
+                <div className='col-lg-4 col-md-6 col-12'>
+                    <p>Search vacancies</p>
+                </div>
             </div>
         </div>
     </div>
