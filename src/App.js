005f33227fc1c4e8372177860c450ed80fa4724f
pranta <prantaacharjee647@gmail.com>
2022-07-27 10:46:27 +0600

    search teacher and profile page added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 import MainLayout from './pages/homePage/mainLayout/MainLayout';
+import SearchLayout from './pages/searchTeachers/serachLayout/SearchLayout';
 import Footer from './pages/shared/footer/Footer';
 import Header from './pages/shared/header/Header';
+import ProfileLayout from './pages/teachersProfile/profileLayout/ProfileLayout';
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       <BrowserRouter>
       <Header></Header>
       <Routes>
-      <Route path='/' element={<MainLayout/>}/> 
+      <Route path='/' element={<MainLayout/>}/>
+      <Route path='/search_teacher' element={<SearchLayout/>}/> 
+      <Route path='/teacher_profile' element={<ProfileLayout/>}/> 
       </Routes>
       <Footer></Footer>      
       </BrowserRouter>      
---
+++ src/pages/searchTeachers/serachLayout/SearchLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchLayout = () => {
+  return (
+    <div>SearchLayout</div>
+  )
+}
+
+export default SearchLayout
