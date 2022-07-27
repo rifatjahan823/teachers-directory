a8ad6c8d69277538b5022b0bc95c1ce2e1aaf9d6
pranta <prantaacharjee647@gmail.com>
2022-07-27 12:05:01 +0600

    header done

--- src/pages/shared/header/Header.jsx
+++ src/pages/shared/header/Header.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import "./Header.css";
 
-const Header = () => {
+const Header=()=>{
   return (
-    <div>Header</div>
-  )
+    <Navbar style={{background:'white'}} expand="lg">
+      <Container fluid>
+        <Navbar.Brand>Td Teachers' Directory</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+             
+          </Nav>           
+           <Link className='header_link' to='/'>Search teachers</Link>
+           <Link className='header_link' to='/'>Job search</Link>
+           <Link className='header_link' to='/'>How it works</Link>
+           <Link className='header_link' to='/'>About us</Link>
+           <Link className='header_link' to='/'>Contact us</Link>
+           <Link className='header_link' to='/'>
+           <Button variant="outline-info px-4">Login</Button>
+           </Link>
+           <Link className='header_link' to='/'>
+           <Button>Register</Button>
+           </Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
+
+
+
+{/*  */}
